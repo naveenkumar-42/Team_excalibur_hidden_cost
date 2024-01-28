5e09e10c20377345e147387a662fa3e41c6688f1
naveenkumar-42 <naveenkumarpoff@gmail.com>
2024-01-28 20:38:27 +0530

    final

--- background.js
+++ background.js
@@ -1,19 +1,19 @@
 let isHighlighting = false;
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action === 'turnOnHighlighting') {
-    isHighlighting = true; // Turn on highlighting
-  } else if (request.action === 'turnOffHighlighting') {
-    isHighlighting = false; // Turn off highlighting
+  if (request.action === 'toggleHighlighting') {
+    isHighlighting = !isHighlighting; // Toggle highlighting
   }
 
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     const activeTab = tabs[0];
+    
+    // Send a single message to the content script to toggle highlighting
     chrome.tabs.sendMessage(activeTab.id, { action: 'toggleHighlighting', isHighlighting });
-  });
 
-  // Save the highlighting state in storage
-  chrome.storage.local.set({ isHighlighting: isHighlighting });
+    // Save the highlighting state in storage
+    chrome.storage.local.set({ isHighlighting: isHighlighting });
+  });
 });
 
 // Retrieve the highlighting state from storage on extension startup
