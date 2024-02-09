69d5df9c5fccc7f1c8dedab74482350a07aaa5cc
naveenkumar-42 <naveenkumarpoff@gmail.com>
2024-02-09 18:40:19 +0530

    update

--- background.js
+++ background.js
@@ -55,3 +55,17 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     });
   }
 });
+
+// background.js
+chrome.runtime.onInstalled.addListener(function () {
+  console.log("Dark Pattern Extension installed");
+});
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.action === "openNewTab") {
+    const mlUrl = "http://127.0.0.1:5000/";  // Replace with your ML server URL
+    const newTabUrl = mlUrl + "?url=" + encodeURIComponent(message.url);
+    chrome.tabs.create({ url: newTabUrl });
+
+  }
+});
--- hiddencost.js
+++ hiddencost.js
@@ -112,3 +112,12 @@ let backButton = document.querySelector('.back_button');
 backButton.addEventListener('click', function() {
   chrome.runtime.sendMessage({action: 'backButtonClicked'});
 });
+
+
+// popup.js
+document.getElementById('urlForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+  const url = document.getElementById('urlInput').value;
+
+  chrome.runtime.sendMessage({ action: 'openNewTab', url: url });
+});
